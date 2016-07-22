daea4fced5e3da4bd14533cfb7c0e40c7d81ceff
fxst24 <matu_hiro@i.softbank.jp>
2016-07-22 13:00:38 +0900

    test

--- mylib.py
+++ mylib.py
@@ -5,11 +5,13 @@ import forex_system
 import numpy as np
 import pandas as pd
 import struct
+import tensorflow as tf
 import time
 
 from collections import OrderedDict
 from datetime import datetime
 from numba import float64, jit
+from pandas_datareader import data as web
 from scipy.optimize import curve_fit
 
 def calc_accuracy4win(*args):
@@ -216,6 +218,368 @@ def calc_volatilities(*args):
     print("1時間足のボラティリティ = ", vola60)
     print("4時間足のボラティリティ = ", vola240)
     print("日足のボラティリティ = ", vola1440)
+
+def check_google_report1():
+    '''Googleのレポートを検証する（①）。
+    '''
+
+    # ヒストリカルデータの開始日と終了日を設定する。
+    start = datetime(2010, 1, 1)
+    end = datetime(2015, 10, 1)
+     
+    # ヒストリカルデータをダウンロードする。
+    snp = web.DataReader('^GSPC', 'yahoo', start, end)
+    nyse = web.DataReader('^NYA', 'yahoo', start, end)
+    djia = web.DataReader('^DJI', 'yahoo', start, end)
+    nikkei = web.DataReader('^N225', 'yahoo', start, end)
+    hangseng = web.DataReader('000001.SS', 'yahoo', start, end)
+    ftse = web.DataReader('^FTSE', 'yahoo', start, end)
+    dax = web.DataReader('^GDAXI', 'yahoo', start, end)
+    aord = web.DataReader('^AORD', 'yahoo', start, end)
+     
+    # 終値を格納する。
+    closing_data = pd.DataFrame()
+    closing_data['snp_close'] = snp['Close']
+    closing_data['nyse_close'] = nyse['Close']
+    closing_data['djia_close'] = djia['Close']
+    closing_data['nikkei_close'] = nikkei['Close']
+    closing_data['hangseng_close'] = hangseng['Close']
+    closing_data['ftse_close'] = ftse['Close']
+    closing_data['dax_close'] = dax['Close']
+    closing_data['aord_close'] = aord['Close']
+     
+    # 終値の欠損値を補間する。
+    closing_data = closing_data.fillna(method='ffill')
+     
+    # 終値の対数変化率を格納する。
+    log_return_data = pd.DataFrame()
+    log_return_data['snp_log_return'] = (
+    np.log(closing_data['snp_close'] /
+        closing_data['snp_close'].shift()))
+    log_return_data['nyse_log_return'] = (np.log(closing_data['nyse_close'] /
+        closing_data['nyse_close'].shift()))
+    log_return_data['djia_log_return'] = (np.log(closing_data['djia_close'] /
+        closing_data['djia_close'].shift()))
+    log_return_data['nikkei_log_return'] = (np.log(closing_data['nikkei_close'] /
+        closing_data['nikkei_close'].shift()))
+    log_return_data['hangseng_log_return'] = (
+        np.log(closing_data['hangseng_close'] /
+        closing_data['hangseng_close'].shift()))
+    log_return_data['ftse_log_return'] = (np.log(closing_data['ftse_close'] /
+        closing_data['ftse_close'].shift()))
+    log_return_data['dax_log_return'] = (np.log(closing_data['dax_close'] /
+        closing_data['dax_close'].shift()))
+    log_return_data['aord_log_return'] = (np.log(closing_data['aord_close'] /
+        closing_data['aord_close'].shift()))
+     
+    # S&P500の対数変化率が0以上なら1、さもなければ0を格納した列を加える。
+    log_return_data['snp_log_return_positive'] = 0
+    log_return_data.ix[log_return_data['snp_log_return'] >= 0,
+                       'snp_log_return_positive'] = 1
+     
+    # S&P500の対数変化率が0未満なら1、さもなければ0を格納した列を加える。
+    log_return_data['snp_log_return_negative'] = 0
+    log_return_data.ix[log_return_data['snp_log_return'] < 0,
+                       'snp_log_return_negative'] = 1
+     
+    # 学習・テスト用データを作成する。
+    training_test_data = pd.DataFrame(
+        columns=[
+            'snp_log_return_positive', 'snp_log_return_negative',
+            'snp_log_return_1', 'snp_log_return_2', 'snp_log_return_3',
+            'nyse_log_return_1', 'nyse_log_return_2', 'nyse_log_return_3',
+            'djia_log_return_1', 'djia_log_return_2', 'djia_log_return_3',
+            'nikkei_log_return_1', 'nikkei_log_return_2', 'nikkei_log_return_3',
+            'hangseng_log_return_1', 'hangseng_log_return_2', 'hangseng_log_return_3',
+            'ftse_log_return_1', 'ftse_log_return_2', 'ftse_log_return_3',
+            'dax_log_return_1', 'dax_log_return_2', 'dax_log_return_3',
+            'aord_log_return_1', 'aord_log_return_2', 'aord_log_return_3'])
+    for i in range(7, len(log_return_data)):
+        snp_log_return_positive = log_return_data['snp_log_return_positive'].ix[i]
+        snp_log_return_negative = log_return_data['snp_log_return_negative'].ix[i]
+        # 先読みバイアスを排除するため、当日のデータを使わない。
+        snp_log_return_1 = log_return_data['snp_log_return'].ix[i-1]
+        snp_log_return_2 = log_return_data['snp_log_return'].ix[i-2]
+        snp_log_return_3 = log_return_data['snp_log_return'].ix[i-3]
+        nyse_log_return_1 = log_return_data['nyse_log_return'].ix[i-1]
+        nyse_log_return_2 = log_return_data['nyse_log_return'].ix[i-2]
+        nyse_log_return_3 = log_return_data['nyse_log_return'].ix[i-3]
+        djia_log_return_1 = log_return_data['djia_log_return'].ix[i-1]
+        djia_log_return_2 = log_return_data['djia_log_return'].ix[i-2]
+        djia_log_return_3 = log_return_data['djia_log_return'].ix[i-3]
+        nikkei_log_return_1 = log_return_data['nikkei_log_return'].ix[i-1]
+        nikkei_log_return_2 = log_return_data['nikkei_log_return'].ix[i-2]
+        nikkei_log_return_3 = log_return_data['nikkei_log_return'].ix[i-3]
+        hangseng_log_return_1 = log_return_data['hangseng_log_return'].ix[i-1]
+        hangseng_log_return_2 = log_return_data['hangseng_log_return'].ix[i-2]
+        hangseng_log_return_3 = log_return_data['hangseng_log_return'].ix[i-3]
+        ftse_log_return_1 = log_return_data['ftse_log_return'].ix[i-1]
+        ftse_log_return_2 = log_return_data['ftse_log_return'].ix[i-2]
+        ftse_log_return_3 = log_return_data['ftse_log_return'].ix[i-3]
+        dax_log_return_1 = log_return_data['dax_log_return'].ix[i-1]
+        dax_log_return_2 = log_return_data['dax_log_return'].ix[i-2]
+        dax_log_return_3 = log_return_data['dax_log_return'].ix[i-3]
+        aord_log_return_1 = log_return_data['aord_log_return'].ix[i-1]
+        aord_log_return_2 = log_return_data['aord_log_return'].ix[i-2]
+        aord_log_return_3 = log_return_data['aord_log_return'].ix[i-3]
+        training_test_data = training_test_data.append(
+            {'snp_log_return_positive':snp_log_return_positive,
+            'snp_log_return_negative':snp_log_return_negative,
+            'snp_log_return_1':snp_log_return_1,
+            'snp_log_return_2':snp_log_return_2,
+            'snp_log_return_3':snp_log_return_3,
+            'nyse_log_return_1':nyse_log_return_1,
+            'nyse_log_return_2':nyse_log_return_2,
+            'nyse_log_return_3':nyse_log_return_3,
+            'djia_log_return_1':djia_log_return_1,
+            'djia_log_return_2':djia_log_return_2,
+            'djia_log_return_3':djia_log_return_3,
+            'nikkei_log_return_1':nikkei_log_return_1,
+            'nikkei_log_return_2':nikkei_log_return_2,
+            'nikkei_log_return_3':nikkei_log_return_3,
+            'hangseng_log_return_1':hangseng_log_return_1,
+            'hangseng_log_return_2':hangseng_log_return_2,
+            'hangseng_log_return_3':hangseng_log_return_3,
+            'ftse_log_return_1':ftse_log_return_1,
+            'ftse_log_return_2':ftse_log_return_2,
+            'ftse_log_return_3':ftse_log_return_3,
+            'dax_log_return_1':dax_log_return_1,
+            'dax_log_return_2':dax_log_return_2,
+            'dax_log_return_3':dax_log_return_3,
+            'aord_log_return_1':aord_log_return_1,
+            'aord_log_return_2':aord_log_return_2,
+            'aord_log_return_3':aord_log_return_3},
+            ignore_index=True)
+     
+    # 3列目以降を説明変数として格納する。
+    predictors_tf = training_test_data[training_test_data.columns[2:]]
+     
+    # 1、2列目を目的変数として格納する。
+    classes_tf = training_test_data[training_test_data.columns[:2]]
+     
+    # 学習用セットのサイズを学習・テスト用データの80%に設定する。
+    training_set_size = int(len(training_test_data) * 0.8)
+     
+    # 説明変数の初めの80%を学習用データにする。
+    training_predictors_tf = predictors_tf[:training_set_size]
+     
+    # 目的変数の初めの80%を学習用データにする。
+    training_classes_tf = classes_tf[:training_set_size]
+     
+    # 説明変数の残りの20%をテスト用データにする。
+    test_predictors_tf = predictors_tf[training_set_size:]
+     
+    # 目的変数の残りの20%をテスト用データにする。
+    test_classes_tf = classes_tf[training_set_size:]
+     
+    def tf_confusion_metrics(model, actual_classes, session, feed_dict):
+        '''正解率等を表示する。
+          Args:
+              model: 
+              actual_classes: 
+              session: 
+              feed_dict: 
+        '''
+        predictions = tf.argmax(model, 1)
+        actuals = tf.argmax(actual_classes, 1)
+        ones_like_actuals = tf.ones_like(actuals)
+        zeros_like_actuals = tf.zeros_like(actuals)
+        ones_like_predictions = tf.ones_like(predictions)
+        zeros_like_predictions = tf.zeros_like(predictions)
+        tp_op = tf.reduce_sum(
+            tf.cast(
+                tf.logical_and(
+                tf.equal(actuals, ones_like_actuals), 
+                tf.equal(predictions, ones_like_predictions)
+                ), 
+                "float"
+            )
+        )
+        tn_op = tf.reduce_sum(
+            tf.cast(
+                tf.logical_and(
+                tf.equal(actuals, zeros_like_actuals), 
+                tf.equal(predictions, zeros_like_predictions)
+                ), 
+                "float"
+             )
+        )    
+        fp_op = tf.reduce_sum(
+            tf.cast(
+                tf.logical_and(
+                tf.equal(actuals, zeros_like_actuals), 
+                tf.equal(predictions, ones_like_predictions)
+                ), 
+                "float"
+            )
+        )    
+        fn_op = tf.reduce_sum(
+            tf.cast(
+                tf.logical_and(
+                tf.equal(actuals, ones_like_actuals), 
+                tf.equal(predictions, zeros_like_predictions)
+                ), 
+                "float"
+            )
+        )
+        tp, tn, fp, fn = \
+            session.run(
+                [tp_op, tn_op, fp_op, fn_op], 
+                feed_dict
+            )
+        tpr = float(tp)/(float(tp) + float(fn))
+        accuracy = ((float(tp) + float(tn))/(float(tp) + float(fp) + float(fn) +
+            float(tn)))
+        recall = tpr
+        precision = float(tp)/(float(tp) + float(fp))  
+        f1_score = (2 * (precision * recall)) / (precision + recall)  
+        print('Precision = ', precision)
+        print('Recall = ', recall)
+        print('F1 Score = ', f1_score)
+        print('Accuracy = ', accuracy)
+     
+    #
+    sess = tf.Session()
+     
+    # 説明変数と目的変数の数を格納する。
+    num_predictors = len(training_predictors_tf.columns)
+    num_classes = len(training_classes_tf.columns)
+     
+    # 
+    feature_data = tf.placeholder("float", [None, num_predictors])
+    actual_classes = tf.placeholder("float", [None, num_classes])
+     
+    # 重みとバイアスを格納する変数を生成する。
+    weights = tf.Variable(
+        tf.truncated_normal([num_predictors, num_classes], stddev=0.0001))
+    biases = tf.Variable(tf.ones([num_classes]))
+     
+    # モデルを定義する。
+    model = tf.nn.softmax(tf.matmul(feature_data, weights) + biases)
+     
+    # コスト関数を定義する。
+    cost = -tf.reduce_sum(actual_classes*tf.log(model))
+     
+    # 学習率を設定する。
+    training_step = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(cost)
+     
+    # 
+    init = tf.initialize_all_variables()
+    sess.run(init)
+     
+    # 正解した予測を格納する。
+    correct_prediction = tf.equal(tf.argmax(model, 1), tf.argmax(actual_classes, 1))
+     
+    # 正解率を格納する。
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+     
+    # 学習を実行する。
+    print('二項分類を用いたモデル')
+    print('学習用データを用いた正解率')
+    for i in range(1, 30001):
+        sess.run(
+            training_step, 
+            feed_dict={
+                feature_data: training_predictors_tf.values, 
+                actual_classes: training_classes_tf.values.reshape(
+                    len(training_classes_tf.values), 2)
+            }
+        )
+        if i%5000 == 0:
+            print(i, sess.run(
+                accuracy,
+                feed_dict={
+                    feature_data: training_predictors_tf.values, 
+                    actual_classes: training_classes_tf.values.reshape(
+                        len(training_classes_tf.values), 2)
+                }
+            ))
+     
+    # 
+    feed_dict= {
+        feature_data: test_predictors_tf.values,
+        actual_classes: test_classes_tf.values.reshape(
+            len(test_classes_tf.values), 2)
+    }
+     
+    #
+    print('テスト用データを用いた検証結果')
+    tf_confusion_metrics(model, actual_classes, sess, feed_dict)
+     
+    #
+    sess1 = tf.Session()
+     
+    # 説明変数と目的変数の数を格納する。
+    num_predictors = len(training_predictors_tf.columns)
+    num_classes = len(training_classes_tf.columns)
+     
+    # 
+    feature_data = tf.placeholder("float", [None, num_predictors])
+    actual_classes = tf.placeholder("float", [None, 2])
+     
+    # 重み1-3、バイアス1-3を格納する変数を生成する。
+    weights1 = tf.Variable(tf.truncated_normal([24, 50], stddev=0.0001))
+    biases1 = tf.Variable(tf.ones([50]))
+    weights2 = tf.Variable(tf.truncated_normal([50, 25], stddev=0.0001))
+    biases2 = tf.Variable(tf.ones([25]))
+    weights3 = tf.Variable(tf.truncated_normal([25, 2], stddev=0.0001))
+    biases3 = tf.Variable(tf.ones([2]))
+     
+    # 隠れ層を生成する。
+    hidden_layer_1 = tf.nn.relu(tf.matmul(feature_data, weights1) + biases1)
+    hidden_layer_2 = tf.nn.relu(tf.matmul(hidden_layer_1, weights2) + biases2)
+     
+    # モデルを定義する。
+    model = tf.nn.softmax(tf.matmul(hidden_layer_2, weights3) + biases3)
+     
+    # コスト関数を定義する。
+    cost = -tf.reduce_sum(actual_classes*tf.log(model))
+     
+    # 学習率を設定する。
+    train_op1 = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(cost)
+     
+    # 
+    init = tf.initialize_all_variables()
+    sess1.run(init)
+     
+    # 正解した予測を格納する。
+    correct_prediction = tf.equal(tf.argmax(model, 1), tf.argmax(actual_classes, 1))
+     
+    # 正解率を格納する。
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+     
+    # 学習を実行する。
+    print('隠れ層を加えたモデル')
+    print('学習用データを用いた正解率')
+    for i in range(1, 30001):
+        sess1.run(
+            train_op1, 
+            feed_dict={
+                feature_data: training_predictors_tf.values, 
+                actual_classes: training_classes_tf.values.reshape(
+                    len(training_classes_tf.values), 2)
+            }
+        )
+        if i%5000 == 0:
+            print(i, sess1.run(
+                accuracy,
+                feed_dict = {
+                    feature_data: training_predictors_tf.values, 
+                    actual_classes: training_classes_tf.values.reshape(
+                        len(training_classes_tf.values), 2)
+                }
+            ))
+     
+    #
+    feed_dict= {
+        feature_data: test_predictors_tf.values,
+        actual_classes: test_classes_tf.values.reshape(
+            len(test_classes_tf.values), 2)
+    }
+     
+    #
+    print('テスト用データを用いた検証結果')
+    tf_confusion_metrics(model, actual_classes, sess1, feed_dict)
 
 def convert_hst2csv():
     '''hstファイルをcsvファイルに変換する。
